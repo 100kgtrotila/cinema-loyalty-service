1f3f709af271d46c416a94ae33ccc446948f85c5
100kgtrotila <hirodevi192@gmail.com>
2026-05-15 18:54:13 +0300

    feat: refactor AppModule and AppService for improved structure and clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { LoyaltyModule } from './loyalty/loyalty.module';
 
 @Module({
   imports: [
-    // Config
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    LoyaltyModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
