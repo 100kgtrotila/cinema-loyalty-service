5bacce2a721033a8cf81933c5cdc12fed6ddf52d
100kgtrotila <hirodevi192@gmail.com>
2026-05-17 01:44:19 +0300

    feat: add loyalty constants for tier thresholds and multipliers

---
+++ src/loyalty/constants/loyalty.constants.ts
@@ -0,0 +1,27 @@
+import { Tier } from 'src/generated/prisma/enums';
+
+export const TIME_CONSTANTS = {
+  MS_IN_A_DAY: 1000 * 60 * 60 * 24,
+};
+
+export const LOYALTY_RULES = {
+  BIRTHDAY_WEEK_RADIUS_DAYS: 3,
+  SPECIAL_EVENT_MULTIPLIER: 2.0,
+
+  TIER_THRESHOLDS: {
+    [Tier.GOLD]: { visits: 20, points: 5000 },
+    [Tier.SILVER]: { visits: 8, points: 2000 },
+  },
+
+  TIER_MULTIPLIERS: {
+    [Tier.GOLD]: 2.0,
+    [Tier.SILVER]: 1.5,
+    [Tier.BRONZE]: 1.0,
+  },
+
+  BIRTHDAY_MULTIPLIERS: {
+    [Tier.GOLD]: 5,
+    [Tier.SILVER]: 3,
+    [Tier.BRONZE]: 2,
+  },
+};
