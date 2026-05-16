9a289aaccd61d8eb6e93e094b544c014fc4f972d
100kgtrotila <hirodevi192@gmail.com>
2026-05-17 01:43:25 +0300

    feat: add EventEmitterModule to AppModule for event handling capabilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { LoyaltyModule } from './loyalty/loyalty.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     LoyaltyModule,
+    EventEmitterModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
