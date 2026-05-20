c94420c420249b1803d4387b8508ced516410ec9
100kgtrotila <hirodevi192@gmail.com>
2026-05-20 15:55:15 +0300

    feat: implement LoyaltySchedulerProducer for scheduling loyalty jobs

---
+++ src/loyalty/producers/loyalty-scheduler.producer.ts
@@ -0,0 +1,53 @@
+import { InjectQueue } from '@nestjs/bullmq';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Queue } from 'bullmq';
+import {
+  LOYALTY_QUEUE_NAME,
+  LOYALTY_JOBS,
+  CRON_SCHEDULES,
+} from '../constants/loyalty.constants';
+
+@Injectable()
+export class LoyaltySchedulerProducer implements OnModuleInit {
+  constructor(
+    @InjectQueue(LOYALTY_QUEUE_NAME) private readonly loyaltyQueue: Queue,
+  ) {}
+
+  async onModuleInit() {
+    await this.loyaltyQueue.add(
+      LOYALTY_JOBS.EXPIRE_POINTS,
+      {},
+      {
+        repeat: { pattern: CRON_SCHEDULES.EVERY_NIGHT_03_00 },
+        jobId: `${LOYALTY_JOBS.EXPIRE_POINTS}-schedule`,
+      },
+    );
+
+    await this.loyaltyQueue.add(
+      LOYALTY_JOBS.NOTIFY_EXPIRING,
+      {},
+      {
+        repeat: { pattern: CRON_SCHEDULES.EVERY_NIGHT_04_00 },
+        jobId: `${LOYALTY_JOBS.NOTIFY_EXPIRING}-schedule`,
+      },
+    );
+
+    await this.loyaltyQueue.add(
+      LOYALTY_JOBS.ANNUAL_RESET,
+      {},
+      {
+        repeat: { pattern: CRON_SCHEDULES.FIRST_OF_JAN_00_05 },
+        jobId: `${LOYALTY_JOBS.ANNUAL_RESET}-schedule`,
+      },
+    );
+
+    await this.loyaltyQueue.add(
+      LOYALTY_JOBS.GOLD_RESET,
+      {},
+      {
+        repeat: { pattern: CRON_SCHEDULES.FIRST_OF_MONTH_01_00 },
+        jobId: `${LOYALTY_JOBS.GOLD_RESET}-schedule`,
+      },
+    );
+  }
+}
