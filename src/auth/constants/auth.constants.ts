bf6ca698f2b3f0e6f340d3dcebe23d028f081b17
100kgtrotila <hirodevi192@gmail.com>
2026-05-24 01:10:11 +0300

    feat: add DOTNET_CLAIM_TYPES and ROLES_KEY constants for authentication

---
+++ src/auth/constants/auth.constants.ts
@@ -0,0 +1,7 @@
+export const DOTNET_CLAIM_TYPES = {
+  ROLE: 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role',
+  NAME_IDENTIFIER:
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier',
+};
+
+export const ROLES_KEY = 'roles';
