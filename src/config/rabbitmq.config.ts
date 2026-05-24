a48d0965f40f718c245150994a8325a12aae67ff
100kgtrotila <hirodevi192@gmail.com>
2026-05-25 01:40:42 +0300

    feat: update RabbitMQ configuration to use constants for client name and queue

--- src/config/rabbitmq.config.ts
+++ src/config/rabbitmq.config.ts
@@ -1,15 +1,16 @@
 import { ConfigService } from '@nestjs/config';
 import { ClientProviderOptions, Transport } from '@nestjs/microservices';
+import { INJECTION_TOKENS, LOYALTY_QUEUE_NAME } from 'src/loyalty/constants/loyalty.constants';
 
 export function getRabbitMqConfig(
   configService: ConfigService,
 ): ClientProviderOptions {
   return {
-    name: 'LOYALTY_PUBLISHER',
+    name: INJECTION_TOKENS.RABBITMQ_LOYALTY_CLIENT,
     transport: Transport.RMQ,
     options: {
       urls: [configService.getOrThrow<string>('RMQ_URL')],
-      queue: 'loyalty.events',
+      queue: LOYALTY_QUEUE_NAME,
       queueOptions: { durable: true },
     },
   };
