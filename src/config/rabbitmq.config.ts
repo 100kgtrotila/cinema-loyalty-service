a363e22258ad3c58c52892303aa560d8b42263d7
100kgtrotila <hirodevi192@gmail.com>
2026-06-07 13:46:35 +0300

    fix: update RabbitMQ configuration to use consistent loyalty events queue name

--- src/config/rabbitmq.config.ts
+++ src/config/rabbitmq.config.ts
@@ -2,7 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { ClientProviderOptions, Transport } from '@nestjs/microservices';
 import {
   INJECTION_TOKENS,
-  LOYALTY_QUEUE_NAME,
+  LOYALTY_EVENTS_QUEUE_NAME,
 } from 'src/loyalty/constants/loyalty.constants';
 
 export function getRabbitMqConfig(
@@ -13,7 +13,9 @@ export function getRabbitMqConfig(
     transport: Transport.RMQ,
     options: {
       urls: [configService.getOrThrow<string>('RMQ_URL')],
-      queue: LOYALTY_QUEUE_NAME,
+      queue:
+        configService.get<string>('RMQ_LOYALTY_EVENTS_QUEUE') ??
+        LOYALTY_EVENTS_QUEUE_NAME,
       queueOptions: { durable: true },
     },
   };
